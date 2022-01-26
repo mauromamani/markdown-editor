fdcdcd19bde07fdab8cd054d81c810965a8ab1fe
mauromamani <mauromamani.b@gmail.com>
2022-01-26 15:18:17 -0300

    chore: customize sidebar

--- packages/render/src/components/SideBar.vue
+++ packages/render/src/components/SideBar.vue
@@ -1,25 +1,30 @@
 <template>
-  <nav class="block bg-gray-800">
+  <nav class="block bg-one-dark border-r-2 border-one-dark-100">
     <div class="w-20 flex flex-col p-3 space-y-3">
       <a
         href="#"
-        class="bg-gray-900 text-white flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg"
+        class="bg-one-dark hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg"
       >
         <span class="sr-only">Open</span>
         <!-- Heroicon name: outline/inbox -->
         <svg
-          class="h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          aria-hidden="true"
         >
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
           />
         </svg>
       </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./packages/render/src/**/*.{html,ts,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'one-dark': '#282c34',
+        'one-dark-200': '#30353d',
+        'one-dark-100': '#4a5460',
+      },
+    },
   },
   plugins: [],
 };
