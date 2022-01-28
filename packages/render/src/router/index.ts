51938999c3fcd2236a54807e09391fa6e1ed3297
mauromamani <mauromamani.b@gmail.com>
2022-01-27 23:24:26 -0300

    fix: blank page in compiled desktop app

--- packages/render/src/router/index.ts
+++ packages/render/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Settings from '@/views/Settings.vue';
 
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   linkExactActiveClass: 'bg-one-dark-200',
 });
