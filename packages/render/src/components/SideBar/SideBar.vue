771dcde379e112eca7586bce7a459f668df46337
mauromamani <mauromamani.b@gmail.com>
2022-02-13 23:33:49 -0300

    chore: add tooltip to sidebar icons

--- packages/render/src/components/SideBar/SideBar.vue
+++ packages/render/src/components/SideBar/SideBar.vue
@@ -16,38 +16,59 @@
 <template>
   <nav class="block bg-one-dark-500 border-r-2 border-one-dark-100">
     <div class="w-[50px] flex flex-col items-center p-3 space-y-3">
-      <button
-        class="bg-one-dark-500 hover:bg-one-dark-200 text-green-300 flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
-        type="button"
-        @click.prevent="notesStore.createNote()"
-      >
-        <span class="sr-only">New Note</span>
-        <plus-circle-icon class="h-6 w-6" />
-      </button>
+      <div class="has-tooltip flex items-center">
+        <span
+          class="tooltip rounded shadow-lg p-2 text-sm font-bold bg-one-dark-100 text-green-300 ml-12"
+          >New Note</span
+        >
+        <button
+          class="bg-one-dark-500 hover:bg-one-dark-200 text-green-300 flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
+          type="button"
+          @click.prevent="notesStore.createNote()"
+        >
+          <plus-circle-icon class="h-6 w-6" />
+        </button>
+      </div>
 
-      <router-link
-        :to="{ name: 'home' }"
-        class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
-      >
-        <span class="sr-only">Editor</span>
-        <code-icon class="h-6 w-6" />
-      </router-link>
+      <div class="has-tooltip flex items-center">
+        <span
+          class="tooltip rounded shadow-lg p-2 text-sm font-bold bg-one-dark-100 text-gray-100 ml-12"
+          >Editor</span
+        >
+        <router-link
+          :to="{ name: 'home' }"
+          class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
+        >
+          <code-icon class="h-6 w-6" />
+        </router-link>
+      </div>
 
-      <button
-        class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
-        @click.prevent="sidebarStore.showNotesBar()"
-      >
-        <span class="sr-only">Home</span>
-        <folder-icon class="h-6 w-6" />
-      </button>
+      <div class="has-tooltip flex items-center">
+        <span
+          class="tooltip rounded shadow-lg p-2 text-sm font-bold bg-one-dark-100 text-gray-100 ml-12"
+          >Open File</span
+        >
+        <button
+          class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
+          @click.prevent="sidebarStore.showNotesBar()"
+        >
+          <folder-icon class="h-6 w-6" />
+        </button>
+      </div>
 
-      <router-link
-        :to="{ name: 'setting' }"
-        class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
-      >
-        <span class="sr-only">Setting</span>
-        <cog-icon class="h-6 w-6" />
-      </router-link>
+      <div class="has-tooltip flex items-center">
+        <span
+          class="tooltip rounded shadow-lg p-2 text-sm font-bold bg-one-dark-100 text-gray-100 ml-12"
+          >Settings</span
+        >
+        <router-link
+          :to="{ name: 'setting' }"
+          class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
+        >
+          <span class="sr-only">Setting</span>
+          <cog-icon class="h-6 w-6" />
+        </router-link>
+      </div>
     </div>
   </nav>
 
