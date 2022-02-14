c43732dc31ae7ec25da0e4e93892f0da4a8cc21e
mauromamani <mauromamani.b@gmail.com>
2022-02-13 22:47:03 -0300

    chore: add icons with vue integration

--- packages/render/src/components/SideBar/SideBar.vue
+++ packages/render/src/components/SideBar/SideBar.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+  import {
+    CodeIcon,
+    PlusCircleIcon,
+    FolderIcon,
+    CogIcon,
+  } from '@heroicons/vue/outline';
   import { useNotesStore } from '@/stores/notes';
   import { useSidebarStore } from '@/stores/sidebar';
   import NotesBar from './NotesBar.vue';
@@ -16,41 +22,23 @@
         @click.prevent="notesStore.createNote()"
       >
         <span class="sr-only">New Note</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <plus-circle-icon class="h-6 w-6" />
       </button>
+
+      <router-link
+        :to="{ name: 'home' }"
+        class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
+      >
+        <span class="sr-only">Editor</span>
+        <code-icon class="h-6 w-6" />
+      </router-link>
 
       <button
         class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
         @click.prevent="sidebarStore.showNotesBar()"
       >
         <span class="sr-only">Home</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-          />
-        </svg>
+        <folder-icon class="h-6 w-6" />
       </button>
 
       <router-link
@@ -58,26 +46,7 @@
         class="bg-one-dark-500 hover:bg-one-dark-200 text-white flex-shrink-0 inline-flex items-center justify-center h-10 w-10 rounded-lg"
       >
         <span class="sr-only">Setting</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-          />
-        </svg>
+        <cog-icon class="h-6 w-6" />
       </router-link>
     </div>
   </nav>
--- packages/render/src/main.ts
+++ packages/render/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia';
 import App from '@/App.vue';
 import router from '@/router';
 import '@/assets/styles/tailwind.css';
-import '@/assets/styles/github-markdown.css';
+import '@/assets/styles/github-markdown.min.css';
 
 const app = createApp(App);
 
