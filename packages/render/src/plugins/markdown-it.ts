017b3763d297676f07eacdcd8ec286b94a05fabb
mauromamani <mauromamani.b@gmail.com>
2022-03-24 23:17:52 -0300

    feat: add highlight.js for syantax highlighting

--- packages/render/src/plugins/markdown-it.ts
+++ packages/render/src/plugins/markdown-it.ts
@@ -1,7 +1,22 @@
 import MarkdownIt from 'markdown-it';
 import emoji from 'markdown-it-emoji';
+import hljs from 'highlight.js';
 
-const md = new MarkdownIt();
+import 'highlight.js/styles/nord.css';
+
+const md = new MarkdownIt({
+  highlight: (str, lang) => {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(str, { language: lang }).value;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    return '';
+  },
+});
 
 export const renderMarkdown = (content: string): string => {
   md.use(emoji);
