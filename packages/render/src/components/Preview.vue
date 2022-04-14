ebe21138338d78ade1e47be7aee7dbc6206f5d9d
mauromamani <mauromamani.b@gmail.com>
2022-04-13 23:49:53 -0300

    chore: add tooltip to hide button

--- packages/render/src/components/Preview.vue
+++ packages/render/src/components/Preview.vue
@@ -21,11 +21,17 @@
       v-html="content"
     ></div>
 
-    <button
-      class="absolute top-2 right-28 px-1 py-2 bg-gray-200 hover:bg-gray-300 rounded text-gray-600"
-    >
-      <chevron-double-right-icon class="h-5 w-5" />
-    </button>
+    <div class="absolute top-2 right-28 has-tooltip flex items-center">
+      <span
+        class="tooltip rounded shadow-lg p-2 text-sm font-bold bg-one-dark-100 text-gray-200 ml-12"
+        >Hide Preview</span
+      >
+      <button
+        class="px-1 py-2 bg-gray-200 hover:bg-gray-300 rounded text-gray-600"
+      >
+        <chevron-double-right-icon class="h-5 w-5" />
+      </button>
+    </div>
 
     <button
       class="absolute top-2 right-6 py-2 px-1 bg-gray-200 hover:bg-gray-300 rounded text-sm text-gray-600 font-semibold transition duration-300"
