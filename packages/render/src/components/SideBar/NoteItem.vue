a1281b952571b3cdc4810f8ee026e0dcdc5b44ec
mauromamani <mauromamani.b@gmail.com>
2022-04-14 00:28:29 -0300

    chore: add delete note button

--- packages/render/src/components/SideBar/NoteItem.vue
+++ packages/render/src/components/SideBar/NoteItem.vue
@@ -3,6 +3,7 @@
   import { Note } from '@/interfaces/notes';
   import { useNotesStore } from '@/stores/notes';
   import { useSidebarStore } from '@/stores/sidebar';
+  import { DocumentTextIcon, TrashIcon } from '@heroicons/vue/outline';
 
   interface Props {
     note: Note;
@@ -17,27 +18,22 @@
     notesStore.setCurrentNote(props.note);
     sidebarStore.showNotesBar();
   };
+
+  const deleteNote = () => {
+    console.log({ note: props.note });
+    console.log('note deleted!');
+  };
 </script>
 
 <template>
   <div
-    class="mt-1 flex items-center px-2 py-1 text-gray-100 font-medium space-x-1 cursor-pointer bg-one-dark rounded-lg"
+    class="relative mt-1 flex items-center px-2 py-1 text-gray-100 font-medium space-x-1 cursor-pointer bg-one-dark rounded-lg"
     @click.prevent="setCurrentNote"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-5 w-5 text-blue-300"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-      />
-    </svg>
-    <h2 class="text-base truncate">{{ props.note.title }}</h2>
+    <document-text-icon class="h-5 w-5 text-blue-300" />
+    <h2 class="text-base truncate w-[100%-20px]">{{ props.note.title }}</h2>
+    <div class="absolute right-1" @click.prevent="deleteNote">
+      <trash-icon class="h-5 w-5 text-red-500" />
+    </div>
   </div>
 </template>
