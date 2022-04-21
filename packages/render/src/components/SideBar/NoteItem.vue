ce2d44bab8231bb9a0242b7a232e049839c63fb4
mauromamani <mauromamani.b@gmail.com>
2022-04-20 23:38:10 -0300

    chore: fix icon position

--- packages/render/src/components/SideBar/NoteItem.vue
+++ packages/render/src/components/SideBar/NoteItem.vue
@@ -26,13 +26,13 @@
 
 <template>
   <div
-    class="relative mt-1 flex items-center px-2 py-1 text-gray-100 font-medium space-x-1 cursor-pointer bg-one-dark rounded-lg"
+    class="mt-1 flex items-center px-2 py-1 text-gray-100 font-medium space-x-1 cursor-pointer bg-one-dark rounded-lg w-full"
   >
     <document-text-icon class="h-5 w-5 text-blue-300" />
     <h2 class="text-base truncate w-10/12" @click.prevent="setCurrentNote">
       {{ props.note.title }}
     </h2>
-    <div class="absolute right-1" @click.prevent="deleteNote">
+    <div @click.prevent="deleteNote">
       <trash-icon class="h-5 w-5 text-red-500 hover:text-red-700" />
     </div>
   </div>
